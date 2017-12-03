07631008536940f6ae40740361ca66f07da12531
Adam Basiak <basiakus@gmail.com>
2017-12-03 17:28:47 +0100

    push

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var fs = require('fs');
+var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+	response.setHeader("Content-Type", "text/html; charset=utf-8");
+
+	if (request.method === 'GET' && request.url === '/') {
+
+	fs.readFile('./index.html', function(err, html) {
+	    if (err) throw err; 
+	    response.write(html);
+	  	response.end();
+	 });	 	
+	} else {
+		response.statusCode = 404;
+		fs.readFile('./fox.jpg', function(err, img) {
+			var imagedata = new Buffer(img).toString('base64');
+			response.write('<h1>...Nie połączyłeś się!</h1>');
+			response.write("<img src='data:fox.jpg;base64,"+imagedata+"'/>");
+    		response.end();
+		});
+	}
+});		
+server.listen(8000);
+
+
