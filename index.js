c5a26360a45b751cac348538639e95bc57e64dc2
Adam Basiak <basiakus@gmail.com>
2017-12-05 19:51:05 +0100

    strange

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs');
 var http = require('http');
 var server = http.createServer();
@@ -8,22 +7,22 @@ server.on('request', function(request, response) {
 
 	if (request.method === 'GET' && request.url === '/') {
 
-	fs.readFile('./index.html', function(err, html) {
-	    if (err) throw err; 
-	    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-	    response.write(html);
-	  	response.end();
-	 });	 	
+		fs.readFile('./index.html', function(err, html) {
+			if (err) throw err;
+			response.writeHead(200, {
+				'Content-Type': 'text/html; charset=utf-8'
+			});
+			response.write(html);
+			response.end();
+		});
 	} else {
 		response.statusCode = 404;
 		fs.readFile('./fox.jpg', function(err, img) {
 			var imagedata = new Buffer(img).toString('base64');
 			response.write('<h1>.ssssssssssss..Nie połączyłeś się!</h1>');
-			response.write("<img src='data:fox.jpg;base64,"+imagedata+"'/>");
-    		response.end();
+			response.write("<img src='data:fox.jpg;base64," + imagedata + "'/>");
+			response.end();
 		});
 	}
-});		
+});
 server.listen(8000);
-
-
